e487d2ae12f6658e3d67f3d938d8689d73a41aab
Emilio <Emilio@192.168.1.33>
2020-05-12 20:01:11 +0200

    Después del ejercicio 3

--- src/RecupEv2Daw/Ordenar.java
+++ src/RecupEv2Daw/Ordenar.java
@@ -6,19 +6,19 @@ public class Ordenar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n1, n2;
+		int numero1, numero2;
 		String mensaje;
 		Scanner teclado = new Scanner(System.in);
 		System.out.print("Introduce el primer numero: ");
-		n1 = teclado.nextInt();
+		numero1 = teclado.nextInt();
 		System.out.print("Introduce el segundo numero: ");
-		n2 = teclado.nextInt();
-		if (n1 == n2) {
+		numero2 = teclado.nextInt();
+		if (numero1 == numero2) {
 			mensaje = "Los dos números son iguales";
-		} else if (n1 > n2) {
-			mensaje = "El numero " + n1 + " es mayor que " + n2;
+		} else if (numero1 > numero2) {
+			mensaje = "El numero " + numero1 + " es mayor que " + numero2;
 		} else {
-			mensaje = "El numero " + n2 + " es mayor que " + n1;
+			mensaje = "El numero " + numero2 + " es mayor que " + numero1;
 		}
 		System.out.print(mensaje);
 		teclado.close();
